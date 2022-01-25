1ae9f03ba48cc2276f71026172652443f53caeb7
joacotol <joacochrist@gmail.com>
2022-01-25 16:08:53 -0500

    Final Solution

--- src/main/java/CarsAssemble.java
+++ src/main/java/CarsAssemble.java
@@ -1,13 +1,18 @@
 import java.rmi.activation.ActivationGroup;
 
+// Create class called CarsAssemble
 public class CarsAssemble {
 
+    // Create a method that will calculate the production rate / hour
     public double productionRatePerHour(int speed) {
         // Calculation for the theoretical amount of cars produced based on speed
         int totalCarsProduced = speed * 221;
         // Returns this variable for actual production rate
         double actualTotalProduced;
+        
+        // Checks if the speed variable fits in one of the conditional statements
         if(speed >= 1 && speed <= 4) {
+            // Calcaulates the actual amount produced based on the success rate
             actualTotalProduced = totalCarsProduced * 1.0;
         } else if(speed >= 5 && speed <= 8) {
             actualTotalProduced = totalCarsProduced * (90/100.0);
@@ -24,13 +29,13 @@ public class CarsAssemble {
 
     }
 
+    // Create a method that will calculate the number working items per 
+    // minute using the method productionRatePerHour();
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
-    }
-
-    public static void main(String[] args) {
-        //double user = productionRatePerHour(6);
-        //System.out.println(user);
+        int itemsPerHour = (int)productionRatePerHour(speed);
+        //Calculate the items per minute that is produced based on the variable itemsPerHour
+        int itemsPerMinute = itemsPerHour / 60;
+        return itemsPerMinute;
     }
 }
 
